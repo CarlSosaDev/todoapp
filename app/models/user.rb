befe4a74f6fd9ee40d52958781827a4da2e39020
Carlos Sosa <c.sosa.got@gmail.com>
2017-09-03 11:53:54 -0500

    add attr_accesible to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  has_many :todos       
+  attr_accesible :email, :password, :password_confirmation, :remember_me,:encrypted_password
+  has_many :todos
 end
